89bec8277d299fad9ed05548175345f6f59ebe5c
NehaT97 <nehathakur764@gmail.com>
2021-01-15 12:07:01 +0530

    login UI Of FaceBook

---
+++ app/src/main/java/com/bridgelabz/facebookapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.bridgelabz.facebookapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun registrationPage(view: View) {
+
+        val intent = Intent(this,RegistrationActivity::class.java)
+        startActivity(intent)
+    }
+}
